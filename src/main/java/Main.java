1bc6a25af1906078416e0ed7a856131bad42feaf
Jarek <jjankowski1@gmail.com>
2018-07-12 20:42:26 +0200

    add read collumn

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,41 +6,47 @@ public class Main {
 //        String[] input1 = "123456789".split("");
 //        String[] input2 = "000402203".split("");
 //        String[] input3 = "600000200".split("");
-//        System.out.println(isArrayCorrect(input1));
-//        System.out.println(isArrayCorrect(input2));
-//        System.out.println(isArrayCorrect(input3));
-//        System.out.println(FR.readFromFile("input1.txt"));
-//        String[] inputInArray = FR.readFromFile("input1.txt").split("\n");
-//        System.out.println("------------");
-//
-//        for (int i = 0; i < inputInArray.length; i++) {
-//            System.out.println(inputInArray[i]);
-//        }
+//        System.out.println(isArrayCorrectString(input1));
+//        System.out.println(isArrayCorrectString(input2));
+//        System.out.println(isArrayCorrectString(input3));
 
-        String input55 = "006000200";
-        char[] inputChar = input55.toCharArray();
-        System.out.println(isArrayCorrectChar(inputChar));
+        System.out.println(FR.readFromFile("input1.txt"));
+        String[] inputInArray = FR.readFromFile("input1.txt").split("\n");
+        System.out.println("------------");
+        System.out.println(inputInArray[0]);
+        System.out.println("------------");
+
+        for (int i = 0; i < 9; i++) {
+            System.out.println(readRow(inputInArray, i));
+            System.out.println(isArrayCorrectChar(readRow(inputInArray, i)));
+        }
 
 
     }
 
 
-    private static String readRow(String input, int column) {
-
-        return "";
+    private static char[] readRow(String[] input, int row) {
+        return input[row].toCharArray();
     }
 
-    private static boolean isArrayCorrect(String[] input){
+    private static char[] readCollumn(String[] input, int collumn){
+        char[] output = new char[9];
+        for (int i = 0; i < 9; i++) {
+            output[i] = input[i].charAt(i);
+        }
+        return output;
+    }
+
+    private static boolean isArrayCorrectString(String[] input) {
         for (int i = 0; i < 9; i++) {
             String firstNumber = input[i];
             if (!firstNumber.equals("0")) {
                 for (int j = i + 1; j < 9; j++) {
                     String nextNumber = input[j];
-                    if(!nextNumber.equals("0")){
+                    if (!nextNumber.equals("0")) {
                         if (firstNumber.equals(nextNumber)) {
                             return false;
                         }
-
                     }
                 }
             }
@@ -48,17 +54,16 @@ public class Main {
         return true;
     }
 
-    private static boolean isArrayCorrectChar(char[] input){
+    private static boolean isArrayCorrectChar(char[] input) {
         for (int i = 0; i < 9; i++) {
             char firstNumber = input[i];
             if (firstNumber != '0') {
                 for (int j = i + 1; j < 9; j++) {
                     char nextNumber = input[j];
-                    if(nextNumber != '0'){
+                    if (nextNumber != '0') {
                         if (firstNumber == nextNumber) {
                             return false;
                         }
-
                     }
                 }
             }
