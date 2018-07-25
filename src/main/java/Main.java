c68b6d25b9856aec0f07585cd98446f32aab7e1a
Jarek <jjankowski1@gmail.com>
2018-07-25 23:44:04 +0200

    clean up, works

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,67 @@
-import java.io.FileReader;
+// program checks if Sudoku board is correct
 
 public class Main {
     public static void main(String[] args) {
-//
-//        String[] input1 = "123456789".split("");
-//        String[] input2 = "000402203".split("");
-//        String[] input3 = "600000200".split("");
-//        System.out.println(isArrayCorrectString(input1));
-//        System.out.println(isArrayCorrectString(input2));
-//        System.out.println(isArrayCorrectString(input3));
 
-        System.out.println(FR.readFromFile("input4.txt"));
-        String[] inputInArray = FR.readFromFile("input4.txt").split("\n");
+        System.out.println(FR.readFromFile("input2.txt"));
         System.out.println("------------");
-        System.out.println(readSquere(inputInArray, 1));
-
+        System.out.println(isSudokuBoardCorrect("input2.txt"));
 
     }
 
-    private static char[] readSquere(String[] input, int sq){
+    private static boolean isSudokuBoardCorrect(String fileInput){
+        String[] inputInArray = FR.readFromFile(fileInput).split("\n");
+        for (int i = 0; i < 9; i++) {
+            if(isArrayCorrectChar(readSquere(inputInArray, i)) &&
+            isArrayCorrectChar(readColumn(inputInArray, i)) &&
+            isArrayCorrectChar(readRow(inputInArray, i))) return true;
+        } return false;
+    }
+
+    private static char[] readSquere(String[] input, int square){
         char[] output = new char[9];
         int startX, startY;
-        startX = 3;
-        startY = 0;
+        switch (square+1){
+            case 1:
+                startX = 0;
+                startY = 0;
+                break;
+            case 2:
+                startX = 3;
+                startY = 0;
+                break;
+            case 3:
+                startX = 6;
+                startY = 0;
+                break;
+            case 4:
+                startX = 0;
+                startY = 3;
+                break;
+            case 5:
+                startX = 3;
+                startY = 3;
+                break;
+            case 6:
+                startX = 6;
+                startY = 3;
+                break;
+            case 7:
+                startX = 0;
+                startY = 6;
+                break;
+            case 8:
+                startX = 3;
+                startY = 6;
+                break;
+            case 9:
+                startX = 6;
+                startY = 6;
+                break;
+            default:
+                startX = 0;
+                startY = 0;
+        }
         for (int i = 0; i < 3; i++) {
             output[i] = input[startY].charAt(startX+i);
             output[i+3] = input[startY+1].charAt(startX+i);
@@ -34,31 +73,13 @@ public class Main {
     private static char[] readRow(String[] input, int row) {
         return input[row].toCharArray();
     }
-    
-    
-    private static char[] readCollumn(String[] input, int collumn){
+
+    private static char[] readColumn(String[] input, int column){
         char[] output = new char[9];
         for (int i = 0; i < 9; i++) {
-            output[i] = input[i].charAt(collumn);
+            output[i] = input[i].charAt(column);
         }
         return output;
-    }
-
-    private static boolean isArrayCorrectString(String[] input) {
-        for (int i = 0; i < 9; i++) {
-            String firstNumber = input[i];
-            if (!firstNumber.equals("0")) {
-                for (int j = i + 1; j < 9; j++) {
-                    String nextNumber = input[j];
-                    if (!nextNumber.equals("0")) {
-                        if (firstNumber.equals(nextNumber)) {
-                            return false;
-                        }
-                    }
-                }
-            }
-        }
-        return true;
     }
 
     private static boolean isArrayCorrectChar(char[] input) {
